1f1cfaa16597c136e00c223a5b504c2839b4d91a
Rajath <rajath37@gmail.com>
2021-06-05 13:44:39 +0530

    working chess board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Chessboard from "chessboardjsx";
+import Chess from "chess.js";
+import logo from "./logo.svg";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [fen, setFen] = useState("start");
+  let game = useRef(null);
+  useEffect(() => {
+    game.current = new Chess();
+  }, []);
+  const onDrop = ({ sourceSquare, targetSquare }) => {
+    let move = game.current.move({
+      from: sourceSquare,
+      to: targetSquare,
+    });
+    if (move === null) return;
+
+    setFen(game.current.fen());
+    console.log(fen);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img className="logo" src={logo} alt="chess logo" />
+
+      <div className="container">
+        <Chessboard position={fen} onDrop={onDrop} />
+      </div>
     </div>
   );
 }
